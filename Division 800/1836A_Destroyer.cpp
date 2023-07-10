d8a29b3deb96a14573865def941b5cb50be50452
Atharva961 <atharvaag2002@gmail.com>
2023-07-10 23:05:43 +0530

    25th problem solved

--- Division 800/1836A_Destroyer.cpp
+++ Division 800/1836A_Destroyer.cpp
@@ -1,34 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+ 
 int main()
 {
     int t;
     cin>>t;
-
+ 
     while(t--)
     {
         int n;
         cin>>n;
-
+ 
         vector<int> h(201, 0);
-
+ 
         for(int i=0; i<n; i++)
         {
             int temp;
             cin>>temp;
             h[temp]++;
         }
-
+ 
+        bool ans = true;
         for(int i=1; i<h.size(); i++)
         {
             if(h[i]>h[i-1])
             {
-                cout<<"NO"<<endl;
-                continue;
+                ans = false;
+                break;
             }
         }
-        cout<<"YES"<<endl;
+        if(ans)cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
     }
     return 0;
 }
