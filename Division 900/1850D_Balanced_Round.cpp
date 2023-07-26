a921ecee0641bfe733eb3cf4a9b103bc206cb672
Atharva961 <atharvaag2002@gmail.com>
2023-07-26 22:33:04 +0530

    54 problems solved

--- Division 900/1850D_Balanced_Round.cpp
+++ Division 900/1850D_Balanced_Round.cpp
@@ -3,6 +3,44 @@ using namespace std;
 
 int main()
 {
-    
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int n, k;
+        cin>>n>>k;
+        vector<int> nums;
+
+        for(int i=0; i<n; i++)
+        {
+            int temp;
+            cin>>temp;
+            nums.push_back(temp);
+        }
+
+        sort(nums.begin(), nums.end());
+        vector<int> diff;
+
+        for(int i=1; i<nums.size(); i++)diff.push_back(nums[i] - nums[i-1]);
+
+        int ans = 0, count = 0;
+
+        for(int i=0; i<diff.size(); i++)
+        {
+            if(diff[i] <= k)
+            {
+                count++;
+            }
+            else
+            {
+                ans = max(ans, count);
+                count = 1;
+            }
+        }
+        ans = max(ans, count);
+
+        cout<<n-ans-1<<endl;
+    }
     return 0;
 }
